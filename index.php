8fce9ac5bad985aa887daf7a4c3c27908f3e1b3f
Alex Sandro <alex@creapix.com.br>
2020-06-01 19:44:50 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+            <script src="https://kit.fontawesome.com/c50df8b1a0.js" crossorigin="anonymous"></script>
+            <link rel="stylesheet" type="text/css" href="css/style.css">
+            <title>Projeto 06</title>
+        </head>
+        <body>
+            
+            <section class="main">
+                <div class="layer"></div><!--layer-->
+                <div class="center">
+                    <header>
+                        <div class="icons-social">
+                            <a href="https://facebook.com"><i class="fab fa-facebook-square"></i></a>
+                            <a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+                        </div><!--Icons-social--> 
+                        <div class="clear"></div><!--clear-->
+                        <div class="logo">
+                            Black.Code
+                        </div><!--logo-->
+                        <nav class="desktop">
+                            <ul>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Sobre</a></li>
+                                <li><a href="">Contato</a></li>
+                            </ul>
+                        </nav><!--desktop-->
+                        <div class="clear"></div><!--clear-->
+                    </header> 
+                    <div class="conteudo-header">
+                        <h2>Bem vindo(a) ao nosso <span>Website</span></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis nisi risus, sed hendrerit orci cursus ac. Nunc placerat sagittis dictum. Vestibulum vel rhoncus elit. Nam dignissim, purus et fermentum mattis, nunc sapien dignissim mi, in accumsan massa dolor vitae odio. Sed arcu libero, fringilla nec mollis ut, finibus sit amet neque. Cras pretium est in sapien tristique, ac tincidunt dolor viverra.</p>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis nisi risus, sed hendrerit orci cursus ac. Nunc placerat sagittis dictum. Vestibulum vel rhoncus elit. Nam dignissim, purus et fermentum mattis, nunc sapien dignissim mi, in accumsan massa dolor vitae odio. Sed arcu libero, fringilla nec mollis ut, finibus sit amet neque. Cras pretium est in sapien tristique, ac tincidunt dolor viverra.</p>
+                         <a href="">Saiba mais</a>
+                    </div><!--conteudo-header-->
+                </div><!--center-->
+            </section><!-- main -->
+            <section class="planos">
+                <div class="chamada-planos">
+                    <div class="center">
+                        <h2>Confira nossos planos</h2>
+                    </div><!--center-->
+                </div><!--chamada-planos-->
+                <section class="box-planos">
+                    <div class="center">
+                        <div class="box-planos-single">
+                            <div class="box-planos-wraper">
+                                <h2>Lorem ipsum</h2>
+                                <div class="pontos-plano">
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                </div><!--pontos-plano-->
+                                <div class="preco">
+                                    <h2>R$ 250,00</h2>
+                                </div><!--preco-->
+                            </div><!--box-planos-wraper-->
+                        </div><!--box-planos-single-->
+
+                         <div class="box-planos-single">
+                            <div class="box-planos-wraper">
+                                <h2>Lorem ipsum</h2>
+                                <div class="pontos-plano">
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                </div><!--pontos-plano-->
+                                <div class="preco">
+                                    <h2>R$ 250,00</h2>
+                                </div><!--preco-->
+                            </div><!--box-planos-wraper-->    
+                        </div><!--box-planos-single-->
+                            
+                        <div class="box-planos-single">
+                            <div class="box-planos-wraper">
+                                <h2>Lorem ipsum</h2>
+                                <div class="pontos-plano">
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                    <p><i class="fas fa-check-square"></i> Lorem ipsum silor domor</p>
+                                </div><!--pontos-plano-->
+                                <div class="preco">
+                                    <h2>R$ 250,00</h2>
+                                </div><!--preco-->
+                            </div><!--box-planos-wraper-->
+                        </div><!--box-planos-single-->
+
+                    </div><!--center-->
+                </section><!--box-planos-->
+            </section><!--planos-->
+        </body>
+    </html>
